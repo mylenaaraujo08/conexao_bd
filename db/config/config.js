d560296f521911a08112135ab8358b70b35adf3f
mylena <mylena08@gmail.com>
2023-10-15 21:07:49 -0300

    Conexaobd.js

---
+++ db/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": 131109,
+    "database": "mylen",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": 131109,
+    "database": "mylen",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": 131109,
+    "database": "mylen",
+    "host": "localhost",
+    "dialect": "mysql"
+  }
+}
